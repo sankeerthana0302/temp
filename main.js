ff58c7e92932edfb906046e9732bd6648d50f0a0
sankeerthana0302 <sankeerthana0302@gmail.com>
2025-04-04 11:27:58 +0530

    add

---
+++ main.js
@@ -0,0 +1,14 @@
+import greet from "./calc.js"
+import {add} from "./calc.js"
+let result = greet()
+console.log(result)
+// let result = add(4,5)
+// console.log(result)
+
+
+// import greet from "./calc.js"
+// import {add} from "./calc.js"
+// let result = greet()
+// console.log(result)
+// // let result = add(4,5)
+// // console.log(result)
